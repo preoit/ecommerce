e6191793fd707f59f8d82e934e6916a2bd38803a
preoit <preoit@users.noreply.github.com>
2026-09-10 00:41:54 +0600

    Remove duplicate communication success toast

---
+++ public/build/assets/Form-CnUPzChi.js
@@ -0,0 +1,1 @@
+import{t as e}from"./app-BdYt77aK.js";import{t}from"./CategoryFormModal-AUFCegGS.js";var n=e();function r({category:e=null,parentOptions:r=[],resource:i=`categories`,entityLabel:a=`category`}){return(0,n.jsx)(t,{page:!0,open:!0,category:e,parentOptions:r,resource:i,entityLabel:a})}export{r as default};
