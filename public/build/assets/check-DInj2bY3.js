3eed9b0607b21144108512f93ec2ac76cb58615a
preoit <preoit@users.noreply.github.com>
2026-09-10 22:57:24 +0600

    Compact customer profile summary

--- public/build/assets/check-DInj2bY3.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-DtK_8ddf.js";var t=e(`check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
---
+++ public/build/assets/chevron-right-D9w50ncM.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-CSn9iPdC.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
