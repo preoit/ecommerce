0449f2b9a0a916dc2952c33eebd38b6b38ef1771
preoit <preoit@users.noreply.github.com>
2026-09-10 23:09:19 +0600

    Show order amount breakdown in list

---
+++ public/build/assets/InputError-BsLZl4fI.js
@@ -0,0 +1,1 @@
+import{t as e}from"./app-CD2rZsu1.js";var t=e();function n({message:e,className:n=``,...r}){return e?(0,t.jsx)(`p`,{...r,className:`text-sm text-red-600 `+n,children:e}):null}export{n as t};
