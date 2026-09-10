41ff318c51d329e140fa26d8cb981016a600b990
preoit <preoit@users.noreply.github.com>
2026-09-10 22:47:04 +0600

    Reorganize customer profile overview

--- public/build/assets/Form-DlfOJZFr.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./app-CmWLYlGC.js";import{t}from"./CategoryFormModal-BpGnH9F9.js";var n=e();function r({category:e=null,parentOptions:r=[],resource:i=`categories`,entityLabel:a=`category`}){return(0,n.jsx)(t,{page:!0,open:!0,category:e,parentOptions:r,resource:i,entityLabel:a})}export{r as default};
