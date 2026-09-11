28a912ee8c75daa169f75a2a8df7360fc9f9351a
preoit <preoit@users.noreply.github.com>
2026-09-11 14:21:19 +0600

    Nest ordered products in customer card

--- public/build/assets/circle-check-BYrm0qZb.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-G6xX5J0Y.js";var t=e(`circle-check`,[[`circle`,{cx:`12`,cy:`12`,r:`10`,key:`1mglay`}],[`path`,{d:`m16 9-5.5 5.5L8 12`,key:`xofnsj`}]]);export{t};
--- public/build/assets/plus-nk54K8w8.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-G6xX5J0Y.js";var t=e(`plus`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`M12 5v14`,key:`s699le`}]]);export{t};
