62044364e1efe05ba4ab862765ef8d13270fc609
preoit <preoit@users.noreply.github.com>
2026-09-11 14:32:52 +0600

    Simplify manual order card layout

--- public/build/assets/Form-B_5Y9N3x.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./app-D1JaVn47.js";import{t}from"./CategoryFormModal-8A8XV1eC.js";var n=e();function r({category:e=null,parentOptions:r=[],resource:i=`categories`,entityLabel:a=`category`}){return(0,n.jsx)(t,{page:!0,open:!0,category:e,parentOptions:r,resource:i,entityLabel:a})}export{r as default};
