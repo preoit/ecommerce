72d510fd593050e06fbe34d53dfc9d916703a90e
preoit <preoit@users.noreply.github.com>
2026-09-12 23:46:45 +0600

    Open product views on storefront

---
+++ public/build/assets/CheckoutLayout-D5Wikyi8.js
@@ -0,0 +1,1 @@
+import{t as e}from"./app-CpkeXfiF.js";import{t}from"./StorefrontLayout-BH4BbgbC.js";var n=e();function r({children:e}){return(0,n.jsx)(t,{children:(0,n.jsx)(`main`,{className:`mx-auto max-w-[1180px] px-4 py-10 sm:px-6 lg:px-8`,children:e})})}export{r as t};
--- public/build/assets/chevron-right-Igqha_-n.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-m3lphH8Q.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
