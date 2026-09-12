929c07c877f6c787dcd2760456eb7039ab0f5c3f
preoit <preoit@users.noreply.github.com>
2026-09-12 23:17:40 +0600

    Compact selected product image thumbnails

---
+++ public/build/assets/chevron-right-BGXUkkUS.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-CSLl2LH-.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
--- public/build/assets/chevron-right-CggDxZrt.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-DTh0JWCN.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
