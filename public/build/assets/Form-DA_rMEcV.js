9d10d07d6003f47316cf27f44a07ade2f990c343
preoit <preoit@users.noreply.github.com>
2026-09-20 01:35:14 +0600

    Fix related products desktop sidebar placement

--- public/build/assets/Form-DA_rMEcV.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./app-gJGTjrdn.js";import{t}from"./CategoryFormModal-DK9ZCK8j.js";var n=e();function r({category:e=null,parentOptions:r=[],resource:i=`categories`,entityLabel:a=`category`}){return(0,n.jsx)(t,{page:!0,open:!0,category:e,parentOptions:r,resource:i,entityLabel:a})}export{r as default};
