705b8da6ff7f79e5b3849decc23fe6a154c08b2f
preoit <preoit@users.noreply.github.com>
2026-09-20 01:46:02 +0600

    Constrain related product sidebar width

--- public/build/assets/CheckoutLayout-CvDoqGPR.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./app-BF3y8b3b.js";import{t}from"./StorefrontLayout-DR8DIk0F.js";var n=e();function r({children:e}){return(0,n.jsx)(t,{children:(0,n.jsx)(`main`,{className:`mx-auto max-w-[1180px] px-4 py-10 sm:px-6 lg:px-8`,children:e})})}export{r as t};
